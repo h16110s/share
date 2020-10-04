b7c8009e6f0abe16e1c5adc19f3f5338a8743915
h16110s <40137996+h16110s@users.noreply.github.com>
2020-10-05 02:11:56 +0900

    Update install_docker.sh

--- 20.04/install_docker.sh
+++ 20.04/install_docker.sh
@@ -13,19 +13,6 @@ sudo apt -y install \
 # Docker公式GPG鍵を追加
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# 鍵の指紋（fingerprint）が「9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88」かを、指紋の最後の8文字を検索することによって検証する
-#   sudo apt-key fingerprint 0EBFCD88
-
-# pub   4096R/0EBFCD88 2017-02-22
-#       フィンガー・プリント = 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
-# uid                  Docker Release (CE deb) <docker@docker.com>
-# sub   4096R/F273FCD8 2017-02-22
-
-
-# stable（安定版）リポジトリをセットアップする。「amd64」は、x86_64（＝64 bitのARMv8）のこと
-# （「uname -m」コマンドを実行すると、「x86_64」と表示される）。
-# 他には「armhf」（＝32 bitのARMv7、Raspberry Piなど）」や「s390x」（＝IBM z Systems）がある。
-# 「lsb_release -cs」コマンドは「xenial」（参照：前掲の脚注*3）となる。
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 
